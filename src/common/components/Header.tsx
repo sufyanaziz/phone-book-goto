02c4756a4e51608e79a4e2b0c8a0e5f374bf8c83
Achmad Sufyan Aziz <achmadsufyan98@gmail.com>
2023-09-22 14:00:36 +0700

    fix: adjust some style and components

--- src/common/components/Header.tsx
+++ src/common/components/Header.tsx
@@ -1,5 +1,7 @@
 import { Theme, css } from "@emotion/react";
 import Text from "./Text";
+import { NavLink } from "react-router-dom";
+import { TEXT_SIZE, TEXT_TYPE } from "@common/constants/text";
 
 const Header = () => {
   const headerStyle = (theme: Theme) => {
@@ -14,14 +16,44 @@ const Header = () => {
       position: "sticky",
       top: 0,
       zIndex: 1,
+      ".text-link": {
+        fontSize: "12px",
+        display: "flex",
+        color: theme.colors.primaryColor,
+        textDecoration: "none",
+        ":hover": {
+          textDecoration: "underline",
+        },
+        "&.active": {
+          fontFamily: TEXT_TYPE.bold,
+          textDecoration: "underline",
+        },
+        "@media (min-width: 576px)": {
+          fontSize: TEXT_SIZE.medium,
+        },
+      },
     });
   };
   return (
     <header css={headerStyle}>
       <Text text="Phone Book Project" type="bold" />
-      <div css={{ display: "flex" }}>
-        <Text text="Contact List" margin="0 12px 0 0" />
-        <Text text="Form Contact" />
+      <div css={{ display: "flex", gap: "8px" }}>
+        <NavLink
+          to="/"
+          className={({ isActive, isPending }) =>
+            `text-link ${isPending ? "pending" : isActive ? "active" : ""}`
+          }
+        >
+          Contact List
+        </NavLink>
+        <NavLink
+          to="/form"
+          className={({ isActive, isPending }) =>
+            `text-link ${isPending ? "pending" : isActive ? "active" : ""}`
+          }
+        >
+          Form Contact
+        </NavLink>
       </div>
     </header>
   );
--- src/common/components/HeaderTitle.tsx
+++ src/common/components/HeaderTitle.tsx
@@ -1,16 +1,14 @@
 import React from "react";
 import Text from "./Text";
 import { SerializedStyles, css } from "@emotion/react";
-import { ArrowLeft } from "assets/icon";
 
 type THeaderTitle = {
   title: string;
   className?: string;
-  onBack?: () => void;
   css?: Partial<SerializedStyles>;
 };
 
-const HeaderTitle: React.FC<THeaderTitle> = ({ title, onBack, ...rest }) => {
+const HeaderTitle: React.FC<THeaderTitle> = ({ title, ...rest }) => {
   const headerTitleStyle = () => {
     return css({
       marginBottom: "16px",
@@ -24,18 +22,12 @@ const HeaderTitle: React.FC<THeaderTitle> = ({ title, onBack, ...rest }) => {
 
   return (
     <div className={rest.className} css={headerTitleStyle}>
-      {typeof onBack === "function" ? (
-        <div className="icon-left" onClick={onBack}>
-          <ArrowLeft />
-        </div>
-      ) : null}
       <Text text={title} size="large" type="bold" margin="0 0 8px 0" />
     </div>
   );
 };
 
 HeaderTitle.defaultProps = {
-  onBack: undefined,
   css: undefined,
 };
 
